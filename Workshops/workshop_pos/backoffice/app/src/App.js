fa0ec8c13ddf2e6710a000938b88ea56ee5a2e1c
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-18 20:35:24 +0700

    Workshop POS EP 9 Upgrade Plan : 08 design sign in page for backOffice

--- Workshops/workshop_pos/backoffice/app/src/App.js
+++ Workshops/workshop_pos/backoffice/app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container m-5">
+      <div className="card">
+        <h5 className="card-header">Sign in to BackOffice</h5>
+        <div className="card-body">
+          <div className="row mb-3">
+            <label for="inputEmail3" className="col-sm-2 col-form-label">
+              Email
+            </label>
+            <div className="col-sm-10">
+              <input type="email" className="form-control" id="inputEmail3" />
+            </div>
+          </div>
+          <div className="row mb-3">
+            <label for="inputPassword3" className="col-sm-2 col-form-label">
+              Password
+            </label>
+            <div className="col-sm-10">
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword3"
+              />
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Sign in
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
