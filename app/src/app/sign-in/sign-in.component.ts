69295daf94381d395e97e3eea07e7052d0183dc2
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-10-12 20:49:13 +0700

    http reponse status & catchError

--- app/src/app/sign-in/sign-in.component.ts
+++ app/src/app/sign-in/sign-in.component.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { error } from 'console';
+import { catchError, of } from 'rxjs';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
 })
@@ -22,6 +25,43 @@ export class SignInComponent {
         text: 'โปรดกรอก username หรือ password ด้วย',
         icon: 'error',
       });
+    } else {
+      const payload = {
+        username: this.username,
+        password: this.password,
+      };
+
+      try {
+        this.http
+          .post('http://localhost:3000/api/user/signin', payload)
+          .pipe(
+            catchError((error) => {
+              return of({
+                ...error,
+                success: false,
+                message: 'Login failed. Please try again.',
+              }); // Return a default error message
+            })
+          )
+          .subscribe((res: any) => {
+            console.log('Response status:', res.status);
+            console.log('Body:', res.body);
+            if (res.status == 200) {
+            } else {
+              Swal.fire({
+                title: 'ตรวจสอบข้อมูล',
+                text: 'username invalid',
+                icon: 'error',
+              });
+            }
+          });
+      } catch (error: any) {
+        Swal.fire({
+          title: 'error',
+          text: error.message,
+          icon: 'error',
+        });
+      }
     }
   }
 }
