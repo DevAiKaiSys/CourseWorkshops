e0e171658dbcdfd068406be1cf846496bd3d7c4e
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-10-13 08:41:58 +0700

    add signout button

--- app/src/app/sidebar/sidebar.component.ts
+++ app/src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,11 +12,26 @@ import { AuthService } from '../auth/auth.service';
 })
 export class SidebarComponent implements OnInit {
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   name: string = '';
 
   ngOnInit(): void {
     this.name = this.authService.getUsername()!;
-    console.log(this.name);
+  }
+
+  async signout() {
+    const button = await Swal.fire({
+      title: 'ออกจากระบบ',
+      text: 'คุณต้องการออกจากระบบ ใช่หรือไม่',
+      icon: 'question',
+      showCancelButton: true,
+      showConfirmButton: true,
+    });
+
+    if (button.isConfirmed) {
+      this.authService.logout();
+      this.router.navigate(['/login']);
+    }
   }
 }
