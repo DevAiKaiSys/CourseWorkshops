52f3c3614d2253647568683b6ed8e0855a26a6e1
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-10-28 23:07:20 +0700

    new nuxt project frontend & tailwindcss

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    apiSecret: "", // can be overridden by NUXT_API_SECRET environment variable
+    public: {
+      apiBase: "", // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+    },
+  },
+});
