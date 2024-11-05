98a092aea41d5b09f8be729eb5f65aa7c03c2a60
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-11-05 23:24:45 +0700

    Part 7 Add Item to CartItems Increase/Decrease Quantity Remove CartItem

---
+++ RestaurantPOS/Models/CartModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RestaurantPOS.Models
+{
+    public partial class CartModel : ObservableObject
+    {
+        public int ItemId { get; set; }
+        public required string Name { get; set; }
+        public required string Icon { get; set; }
+        public decimal Price { get; set; }
+        [ObservableProperty, NotifyPropertyChangedFor(nameof(Amount))]
+        public int _quantity;
+
+        public decimal Amount => Price * Quantity;
+    }
+}
--- RestaurantPOS/ViewModels/HomeViewModel.cs
+++ RestaurantPOS/ViewModels/HomeViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using RestaurantPOS.Data;
 using RestaurantPOS.Models;
+using System.Collections.ObjectModel;
 using MenuItem = RestaurantPOS.Data.MenuItem;
 
 namespace RestaurantPOS.ViewModels
@@ -20,6 +21,8 @@ namespace RestaurantPOS.ViewModels
 
         [ObservableProperty]
         private MenuCategoryModel? _selectedCategory = null;
+
+        public ObservableCollection<CartModel> CartItems { get; set; } = [];
 
         [ObservableProperty]
         private bool _isLoading;
@@ -101,5 +104,53 @@ namespace RestaurantPOS.ViewModels
         //    category.IsSelected = true;
         //    SelectedCategory
         //}
+
+        [RelayCommand]
+        private void AddToCart(MenuItem menuItem)
+        {
+            CartModel? cartItem = CartItems.FirstOrDefault(c => c.ItemId == menuItem.Id);
+            if (cartItem == null)
+            {
+                // Item does not exist in the cart
+                // Add item to cart
+                cartItem = new CartModel
+                {
+                    ItemId = menuItem.Id,
+                    Icon = menuItem.Icon,
+                    Name = menuItem.Name,
+                    Price = menuItem.Price,
+                    Quantity = 1
+                };
+                CartItems.Add(cartItem);
+            }
+            else
+            {
+                // This item exists in cart
+                // Increase the quantity for this item in the cart
+                cartItem.Quantity++;
+            }
+        }
+
+        [RelayCommand]
+        private void IncreaseQuantity(CartModel cartItem)
+        {
+            cartItem.Quantity++;
+        }
+
+        [RelayCommand]
+        private void DecreaseQuantity(CartModel cartItem)
+        {
+            cartItem.Quantity--;
+            if (cartItem.Quantity == 0)
+            {
+                _ = CartItems.Remove(cartItem);
+            }
+        }
+
+        [RelayCommand]
+        private void RemoveItemFromCart(CartModel cartItem)
+        {
+            _ = CartItems.Remove(cartItem);
+        }
     }
 }
